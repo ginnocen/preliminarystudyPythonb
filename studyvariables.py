61a2b712715f1d8fe6ccb374d1c67ee085b13e6b
Mattia Faggin <mattia.faggin@cern.ch>
2018-10-02 17:04:24 +0200

    New updates from Mattia I just updated a bit the code, making it producing scatter-plots with the Pearson correlation coefficient in the title of each subpad. The plots are saved inside directories that are properly produced in case they do not exist. I also explored the creation of functions by the user, in particular creating a ProgressBar function, that I find useful to follow the saving status of the plots.

--- studyvariables.py
+++ studyvariables.py
@@ -10,12 +10,28 @@ import pickle
 import sys, os
 from timeit import default_timer as timer
 from datetime import datetime
-
-# import the scatter_matrix functionality
-#from pandas.plotting import scatter_matrix
-
 #from ROOT import TNtuple
 #from ROOT import TH1F, TH2F, TCanvas, TFile, gStyle, gROOT
+
+#__________________________________________________________________________________
+def CheckDir(path):
+        if not os.path.exists(path):
+                os.makedirs(path)
+#__________________________________________________________________________________
+def ProgressBar(part,tot):
+        sys.stdout.flush()      # deletes what dumped in the line
+        length=100
+        perc = part/tot
+        num_dashes = int(length*perc)
+        print("\r[",end='')     # go at the beginning of the line and start writing
+        # dashes (completed)
+        for i in range(0,num_dashes+1):
+                print("#",end='')
+        # tick (missing)
+        for i in range(0,length-num_dashes-1):
+                print("-",end='')
+        print("] {0:.0%}".format(perc),end='')
+#__________________________________________________________________________________
 
 time0 = datetime.now()
           
@@ -37,7 +53,7 @@ print(train_set.columns)        # .columns returns an Index type with the indice
 #   the function .los retrieves a column of the DataFrame, #
 #   as a  DataFrame as well (Series)                       #
 ############################################################
-train_set_sig=train_set.loc[train_set['signal_ML'] == 1]        
+train_set_sig=train_set.loc[train_set['signal_ML'] == 1]        # .loc function used to return a DataFrame with all the info about the entries satisfying the condition in brackets     
 train_set_bkg=train_set.loc[train_set['signal_ML'] == 0]
 print("\n --- train_set_sig type: %s\n --- train_set_bkg type: %s\n"%(type(train_set_sig),type(train_set_bkg)))         # train_set_sig and train_set_bkg are DataFrame (Series) as well
 
@@ -82,13 +98,19 @@ for i_subpad in range(1,num_variables):                 # NB: the last variable 
         plt.ylabel("entries",fontsize=11)               # equivalent to s_pad.set_ylabel
         plt.yscale('log')
         kwargs = dict(alpha=0.3,density=True, bins=100)
-        n, bins, patches = plt.hist(train_set_sig[name_var], facecolor='r', label='signal', **kwargs)
+        n, bins, patches = plt.hist(train_set_sig[name_var], facecolor='r', label='signal', **kwargs)           # NB: this function returns MORE THAN ONE ARGUMENT
         n, bins, patches = plt.hist(train_set_bkg[name_var], facecolor='b', label='background', **kwargs)
         s_pad.legend()
 plt.subplots_adjust(hspace=0.5)
 #plt.show()     
 plt.savefig("Var_distributions.pdf")#,bbox_inches='tight')
 
+
+# check if there are the directories where to save the files, otherwise create them
+path_Sig = "./Signal"
+path_Bkg = "./Background"
+CheckDir(path_Sig)
+CheckDir(path_Bkg)
 
 '''
 # ------------------------------------------
@@ -117,13 +139,13 @@ print("--- scplot_Bkg type: %s\t dim: %s\t shape: %s"%(type(scplot_Bkg),scplot_B
 for p_sig in scplot_Sig.ravel():
         p_sig.set_xlabel(p_sig.get_xlabel(),fontsize=10, labelpad=20)
         p_sig.set_ylabel(p_sig.get_ylabel(),fontsize=10, labelpad=50, rotation=0)
-#plt.savefig("matrix_Sig.png")
+plt.savefig(path_Sig+"/matrix_Sig.png")
 
 for p_bkg in scplot_Bkg.ravel():
         p_bkg.set_xlabel(p_bkg.get_xlabel(),fontsize=10, labelpad=20)
         p_bkg.set_ylabel(p_bkg.get_ylabel(),fontsize=10, labelpad=50, rotation=0)
-#plt.savefig("matrix_Bkg.png")
-plt.show()
+plt.savefig(path_Bkg+"/matrix_Bkg.png")
+#plt.show()
 '''
 
 # ------------------------------------------
@@ -135,41 +157,53 @@ print("\tScatter plots")
 print("==================================\n")
 
 for i_col in range(1,num_variables):
-        # signal
-        fig_scPlot=plt.figure(figsize=(16,9))
-        
-        namex = train_set_sig.columns[i_col-1]
+        namex = train_set_sig.columns[i_col-1]        
+
+        index_plot_sig = 1+i_col                # signal plots 
+        index_plot_bkg = 1+i_col+num_variables  # background plots
+
+        fig_scPlot     = plt.figure(num=index_plot_sig,figsize=(16,9))       # signal        
         fig_scPlot.suptitle("Scatter plot SIGNAL %s"%namex,fontsize=40)
+        fig_scPlot_BKG = plt.figure(num=index_plot_bkg,figsize=(16,9))       # background    
+        fig_scPlot_BKG.suptitle("Scatter plot BACKGROUND %s"%namex,fontsize=40)    
+
         for i_col2 in range(1,num_variables):               
                 namey = train_set_sig.columns[i_col2-1]
+
+                plt.figure(index_plot_sig)   # moving to figure 1 (signal)
                 s_pad_scPlot = plt.subplot(4,3,i_col2)
                 plt.scatter(train_set_sig[namex],train_set_sig[namey],s=1,c="red",marker=",")
+                # ----- correlation coefficient -----
+                ndarray_corr_sig = np.corrcoef(train_set_sig[namex],train_set_sig[namey])
+                #print("\n=== Correlation %s vs. %s \n\tCorrelation coefficient matrix SIGNAL: \n%s"%(namex,namey,ndarray_corr_sig))  
+                s_pad_scPlot.set_title("Correlation coefficient: %.4f"%ndarray_corr_sig[0,1])
+                # -----------------------------------
                 s_pad_scPlot.set_xlabel(namex)
                 s_pad_scPlot.set_ylabel(namey)
-                plt.subplots_adjust(wspace=0.5, hspace=0.5)
+                plt.subplots_adjust(wspace=0.5, hspace=0.75)
                 plt.grid()
-                #plt.savefig("scatter_SIG_%d.png"%i_col)
-        # background
-        fig_scPlot_BKG=plt.figure(figsize=(16,9))   
-        fig_scPlot_BKG.suptitle("Scatter plot BACKGROUND %s"%namex,fontsize=40)    
-        for i_col2 in range(1,num_variables):               
-                namey = train_set_sig.columns[i_col2-1]
+                plt.savefig(path_Sig+"/scatter_SIG_%d.png"%i_col)
+
+                plt.figure(index_plot_bkg)   # moving to figure 1 (background)
                 fig_scPlot_BKG = plt.subplot(4,3,i_col2)
                 plt.scatter(train_set_bkg[namex],train_set_bkg[namey],s=1,c="blue",marker=",")
+                # ----- correlation coefficient -----
+                ndarray_corr_bkg = np.corrcoef(train_set_bkg[namex],train_set_bkg[namey])
+                #print("\tCorrelation coefficient matrix BACKGROUND: \n%s"%ndarray_corr_bkg)  
+                fig_scPlot_BKG.set_title("Correlation coefficient: %.4f"%ndarray_corr_bkg[0,1])
+                # -----------------------------------
                 fig_scPlot_BKG.set_xlabel(namex)
                 fig_scPlot_BKG.set_ylabel(namey)
-                plt.subplots_adjust(wspace=0.5, hspace=0.5)
+                plt.subplots_adjust(wspace=0.5, hspace=0.75)
                 plt.grid()
-                #plt.savefig("scatter_BKG_%d.png"%i_col) 
-        
-plt.show()      # after calling 'show', the current plot and axis are destroyed (namely: calling plt.savefig you do not save anything!) ---> all the plots produced in the program are plotted all together!
+                plt.savefig(path_Bkg+"/scatter_BKG_%d.png"%i_col)
+                ProgressBar((i_col-1)*(num_variables-1)+i_col2,(num_variables-1)**2)
 
+#plt.show()      # after calling 'show', the current plot and axis are destroyed (namely: calling plt.savefig you do not save anything!) ---> all the plots produced in the program are plotted all together!
 
-
-
-
-
-
+time1 = datetime.now()
+howmuchtime = time1-time0
+print("\n===\n===\tExecution END. Start time: %s\tEnd time: %s\t(%s)\n==="%(time0.strftime('%d/%m/%Y, %H:%M:%S'),time1.strftime('%d/%m/%Y, %H:%M:%S'),howmuchtime))
 
 
 
